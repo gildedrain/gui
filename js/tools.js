930a49562b49a165e2d627a6010893b37c5aff20
Stephen Erwin <steve@petrock.org>
2012-05-21 09:19:19 -0400

    rebuilt modal function

--- js/tools.js
+++ js/tools.js
@@ -112,54 +112,37 @@ $(document).ready(function() {
     $(this).loadDialog();
     return false;
   });
-
-  // Add the loadDialog() function to any jQuery element. This adds all of the
-  // dialog-related HTML to the DOM, binds the close events, and adds the iframe.
-  // See my comments below for an alternative to the iframe technique.
   $.fn.loadDialog = function() {
     $('body').append('<div id="modal" style="display:none;">'+
       '<div class="window" id="dialog">'+
       '<a class="close" title="Close Window">Click to close</a>'+
-      '<span class="null" id="dialog-content">'+
-      '<iframe id="modal-iframe" frameborder="0" scrolling="no" '+
-      'src="'+this.attr('href').replace(/[^\/]+\.aspx/, 'box.aspx') + '" />'+
+      '<span class="null" id="dialog-content">' +
       '</span></div><div id="mask" />'+
     '</div>');
-
-    $('#modal').show();
-    $('#mask').css({
-      'height': $(window).height(),
-      'width':  $(window).width()
-      }).fadeTo('slow',0.8);
-
-    $('#dialog').css({
-      'top': $(window).height()/2 - $('#dialog').height()/2,
-      'left': $(window).width()/2 - $('#dialog').width()/2
-    }).fadeIn(100);
-
-    // Instead of leaving the dialog HTML in the DOM after the dialog is closed,
-    // I decided to just remove the entire <div id="modal" /> entirely. This
-    // removes everything inside also, and leaves the page exactly the way it
-    // was before the dialog was opened.
+    // Change the link's URL to use box.aspx instead, for a plain container
+    var $inBox = $(this).attr('href').replace(/[^\/]+\.aspx/, 'box.aspx');
+    $("#dialog-content").load($inBox);
+    // Don't show the modal unless you've got something to put in it
+    if ($('#dialog-content').length>0) {
+      $('#modal').show();
+      $('#mask').css({
+        'height': $(document).height(),
+        'width':  $(window).width(),
+        'top':0,
+        'left':0
+      }).fadeTo(150, 0.8);
+      $('#dialog').css({
+        'top': $(window).height()/2 - $('#dialog').height(),
+        'left': $(window).width()/2 - $('#dialog').width()/1.5
+      }).fadeIn(100);
+    }
+    // remove everything from the HTML when you close the modal
     $('#dialog .close').add('#mask').click(function() {
       $('#modal').remove();
       return this;
     });
-
-    // I'm still not sure if I think the iframe is really necessary, but there might
-    // be some funky ektron stuff that I'm not fully understanding.  I would probably
-    // do something like this instead:
-    // $('#dialog-content').load(url for box.aspx), function() {
-    //   ...
-    //   show the modal and size it appropriately
-    //   fade in animations
-    // });
-    // The advantage here is that you can wait for the box.aspx content before starting
-    // the fade-in animations, ensuring that the modal is not displayed empty.
   }
 })(jQuery);
-
-
 
 
 // "Verty" a vertical scrolling anchor list animation effect
